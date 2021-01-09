04cd8e0c045245e3bfc506423d979581e779fcea
Tyler Merry <tymerry@gmail.com>
2021-01-09 15:55:05 -0700

    should be okay

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,58 +1,74 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+  <div>
+    <div class="upload">
+      <vue-csv-import
+          v-model="csv"
+          :fields="{rowOne: {required: false, label: 'First Slot'}, rowTwo :{required: true, label: 'Second Slot'}, rowThree: {required: true, label: 'Third Slot'}}"
+      >
+        <vue-csv-toggle-headers></vue-csv-toggle-headers>
+        <vue-csv-errors></vue-csv-errors>
+        <vue-csv-input></vue-csv-input>
+        <vue-csv-map></vue-csv-map>
+      </vue-csv-import>
+    </div>
+    <div class="items">
+      <div v-for="item in csv" class="item" v-bind:key="item.rowOne">
+        <div>{{item.rowOne}}</div>
+        <div>{{item.rowTwo}}</div>
+        <div>{{item.rowThree}}</div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
 export default {
   name: 'HelloWorld',
-  props: {
-    msg: String
+  data: function(){
+      return {
+          csv: "Please upload a CSV"
+      }
   }
 }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
+ .upload{
+     display:flex;
+     flex-direction: column;
+ }
+
+ .items {
+     display: flex;
+     flex-wrap: wrap;
+ }
+
+ .items > .item {
+     width: 44%;
+     margin: 3%;
+ }
+
+ h3 {
+     margin: 40px 0 0;
+ }
+ ul {
+     list-style-type: none;
+     padding: 0;
+ }
+ li {
+     display: inline-block;
+     margin: 0 10px;
+ }
+ a {
+     color: #42b983;
+ }
+
+ @media print {
+     .upload {
+         background-color: black;
+         display: none;
+     }
+ }
+
 </style>
